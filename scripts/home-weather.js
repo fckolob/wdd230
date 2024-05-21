f81244d066d00557a75c2946f4bbb9400854ffc5
fckolob <131497920+fckolob@users.noreply.github.com>
2024-05-20 22:34:31 -0300

    weather info added

--- scripts/home-weather.js
+++ scripts/home-weather.js
@@ -1,8 +1,8 @@
 const currentTemp = document.querySelector("#current-temp");
 const weatherIcon = document.querySelector("#weather-icon");
-const captionDesc = document.querySelector("figcaption");
+const description = document.querySelector("#weather-description");
 
-const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=fbe55b5d6b972ebb3241f821c164c127";
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=-34.86&lon=-55.27&units=imperial&appid=fbe55b5d6b972ebb3241f821c164c127";
 
 async function apiFetch(){
     try {
@@ -21,12 +21,12 @@ async function apiFetch(){
 }
 
  function displayResults(data){
-    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    currentTemp.innerHTML = `${data.main.temp}&deg;F  `;
     let desc = data.weather[0].description;
     let icon = data.weather[0].icon;
     weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${icon}@2x.png`);
-    weatherIcon.setAttribute("alt", desc);
-    captionDesc.textContent = `${desc}`;
+    weatherIcon.setAttribute("alt", "desc");
+    description.textContent = `  -${desc}`;
  
  }
 
